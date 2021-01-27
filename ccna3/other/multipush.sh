c8adea806bef35d6cc807b7f92137410847d1e09
simuns <sih@synack.fo>
2021-01-27 10:47:08 +0000

    Removed loc to make safe in github (done with alias)

--- ccna3/other/multipush.sh
+++ ccna3/other/multipush.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
+shopt -s expand_aliases
+source ~/.bashrc
+cdgit
 echo "-------Pulling Sihrep------"
-cd /home/simuns/git/sihrep/skuli/H2
+cd sihrep/skuli/H2
 git pull
 echo "-------Pulling Github------"
-cd /home/simuns/git/githubrep/ccna_3-4
+cdgit
+cd githubrep/ccna_3-4
 git pull
+cdgit
 echo "press 1 for Sihrep ¤--> Github"
 echo "press 2 for Sihrep <--¤ Github"
 read choise
 if [ "$choise" = "1" ]
 then
    echo "------Merging from Sihrep ¤--> Github"
-   unison -ui text /home/simuns/git/sihrep/skuli/H2/ccna3/ /home/simuns/git/githubrep/ccna_3-4/ccna3/ -force /home/simuns/git/sihrep/skuli/H2/ccna3/   
+   unison -ui text sihrep/skuli/H2/ccna3/ githubrep/ccna_3-4/ccna3/ -force sihrep/skuli/H2/ccna3/   
 else
    echo "------Merging from Sihrep <--¤ Github"
-   unison -ui text /home/simuns/git/sihrep/skuli/H2/ccna3/ /home/simuns/git/githubrep/ccna_3-4/ccna3/ -force /home/simuns/git/githubrep/ccna_3-4/ccna3/
+   unison -ui text sihrep/skuli/H2/ccna3/ githubrep/ccna_3-4/ccna3/ -force githubrep/ccna_3-4/ccna3/
 fi
-cd /home/simuns/git/sihrep/skuli/H2
+cdgit
+cd sihrep/skuli/H2
 echo "Pulled sihrep"
 echo "Commit msg for both repo's"
 read commitmsg
@@ -24,7 +30,8 @@ echo "------Pushing Sihrep------"
 git add *
 git commit -am "$commitmsg"
 git push
-cd /home/simuns/git/githubrep/ccna_3-4
+cdgit
+cd githubrep/ccna_3-4
 echo "------Pushing Github------"
 git add *
 git commit -am "$commitmsg"
